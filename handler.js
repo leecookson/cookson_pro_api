b30bf5683e6c11d67f444d7320b84bb7d44eddcb
Lee Cookson <1561172+leecookson@users.noreply.github.com>
2025-08-27 16:02:09 -0400

    add log to handler

--- handler.js
+++ handler.js
@@ -6,6 +6,8 @@ const logger = loggers().get('handler', process.env.LOG_LEVEL || 'info');
 
 const server = awsServerlessExpress.createServer(app);
 
-export const handler = (event, context) => {
-  return awsServerlessExpress.proxy(server, event, context);
+export const handler = async (event, context) => {
+  logger.info(`Received event: ${event.rawPath}, requestContext.http: ${JSON.stringify(event.requestContext.http)}`);
+  // Use the 'PROMISE' resolution mode for async handlers. This returns a promise that resolves with the response.
+  return awsServerlessExpress.proxy(server, event, context, 'PROMISE').promise;
 };
